d2d038a773769ee16bab2d258a1fc5b26d832bb0
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-06-25 14:33:54 +0900

    デバッグ用途に、発行した SQL を出力する

--- app/repository/postgresql.py
+++ app/repository/postgresql.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URL = (
     "postgresql://postgres:69f27de1009b478d8198b0249a2b7a62@localhost:5432/fastapi_todo"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
