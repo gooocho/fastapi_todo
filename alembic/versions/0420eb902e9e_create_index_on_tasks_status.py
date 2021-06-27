3bd8cbf40543301379fd4c318202c873bbd3bbca
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-06-27 15:26:06 +0900

    tasks テーブルの status 列にインデックスを作成

---
+++ alembic/versions/0420eb902e9e_create_index_on_tasks_status.py
@@ -0,0 +1,24 @@
+"""create index on tasks(status)
+
+Revision ID: 0420eb902e9e
+Revises: 75346f4187b9
+Create Date: 2021-06-27 13:48:51.646233
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0420eb902e9e'
+down_revision = '75346f4187b9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_index('idx_tasks_status', 'tasks', ['status'])
+
+
+def downgrade():
+    op.drop_index('idx_tasks_status', 'tasks')
