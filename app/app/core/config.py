21a30232f9ae5f6cb59d9f184bde1fa736069350
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-06-28 21:33:23 +0900

    不足していた環境変数を追加

--- app/app/core/config.py
+++ app/app/core/config.py
@@ -1,6 +1,6 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, Optional
 
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
