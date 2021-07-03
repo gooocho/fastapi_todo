f30df378073fc072eb769e2902ed33ffd46be789
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-07-04 08:20:50 +0900

    Field によって default 値が必ず定義されるので Optional型ではない、修正

--- app/app/schemas/pager.py
+++ app/app/schemas/pager.py
@@ -7,8 +7,8 @@ class Pager(BaseModel):
     DEFAULT_PAGE = 1
     DEFAULT_PER_PAGE = 100
 
-    page: Optional[int] = Field(DEFAULT_PAGE, ge=1)
-    per_page: Optional[int] = Field(DEFAULT_PER_PAGE, ge=1, le=500)
+    page: int = Field(DEFAULT_PAGE, ge=1)
+    per_page: int = Field(DEFAULT_PER_PAGE, ge=1, le=500)
 
     def offset(self):
         return (self.page - 1) * self.per_page
