6d2c129d2b739def40eb642defbd20a3f827aaf2
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-07-03 20:46:09 +0900

    crud_user_statisticsに docstring を記述

--- app/app/crud/crud_user_statistics.py
+++ app/app/crud/crud_user_statistics.py
@@ -7,10 +7,24 @@ from sqlalchemy.sql.functions import coalesce
 from app.models.assignment import ModelAssignment
 from app.models.task import ModelTask
 from app.models.user import ModelUser
+from app.schemas.task import TaskStatus
 from app.schemas.user import UserId
 
 
-def assigned_task_counts(db: Session, user_ids: List[UserId], status: int):
+def assigned_task_counts(db: Session, user_ids: List[UserId], status: TaskStatus):
+    """
+    ユーザーにアサインされているタスクの件数を集計する。
+    タスクの件数は優先度ごとに分類される。
+
+    Parameters
+    ----------
+    db : sqlalchemy.orm.Session
+        接続するデータベース。
+    user_ids : List[UserId]
+        検索対象にするユーザー群。
+    status : TaskStatus
+        検索するステータス。
+    """
     return (
         db.query(
             ModelAssignment.user_id,
@@ -26,7 +40,29 @@ def assigned_task_counts(db: Session, user_ids: List[UserId], status: int):
     )
 
 
-def status_task_counts(db: Session, status: int, limit: int, offset: int):
+def status_task_counts(db: Session, status: TaskStatus, limit: int, offset: int):
+    """
+    ユーザーにアサインされたタスクのうち、指定されたステータスであるものの件数を計算する。
+    1. アサインされたタスクが多いもの
+    2. ユーザーID が小さいもの
+    の順に検索される。
+
+    Parameters
+    ----------
+    db : sqlalchemy.orm.Session
+        接続するデータベース。
+    status : TaskStatus
+        検索するステータス。
+    limit : int
+        １回の検索で取得する件数の上限。
+    offset : int
+        先頭を読み飛ばす件数。
+
+    returns
+    -------
+    any
+        「ユーザーIDと、アサインされたタスクの数の組」のリスト。
+    """
     subquery = (
         db.query(ModelAssignment.user_id, func.count(ModelTask.id).label("task_count"))
         .join(ModelTask)
