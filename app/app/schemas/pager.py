d4855693a52dd19fabf9d48817b2ce1d3f605fea
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-07-04 08:16:45 +0900

    マジックナンバーに名前を付ける

--- app/app/schemas/pager.py
+++ app/app/schemas/pager.py
@@ -4,8 +4,11 @@ from pydantic import BaseModel, Field
 
 
 class Pager(BaseModel):
-    page: Optional[int] = Field(1, ge=1)
-    per_page: Optional[int] = Field(100, ge=1, le=500)
+    DEFAULT_PAGE = 1
+    DEFAULT_PER_PAGE = 100
+
+    page: Optional[int] = Field(DEFAULT_PAGE, ge=1)
+    per_page: Optional[int] = Field(DEFAULT_PER_PAGE, ge=1, le=500)
 
     def offset(self):
         return (self.page - 1) * self.per_page
