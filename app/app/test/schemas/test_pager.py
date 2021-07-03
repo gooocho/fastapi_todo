6c1ffa9c55e50133591b9ce9fbf9b57da601c733
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-07-03 16:55:41 +0900

    parametrize を使ってテストパターンを増やす

--- app/app/test/schemas/test_pager.py
+++ app/app/test/schemas/test_pager.py
@@ -1,6 +1,15 @@
+import pytest
+
 from app.schemas.pager import Pager
 
 class TestPager:
-    def test_pager_offset(self) -> None:
-        pager = Pager(page=3, per_page=7)
-        assert pager.offset() == 14
+    @pytest.mark.parametrize("page, per_page, offset", [
+        (1, 10, 0),
+        (2, 10, 10),
+        (3, 7, 14),
+        (6, 20, 100),
+        (13, 17, (13 - 1) * 17)
+    ])
+    def test_pager_offset(self, page, per_page, offset) -> None:
+        pager = Pager(page=page, per_page=per_page)
+        assert pager.offset() == offset
