31c1c99b20594a5266da53e5a8075e911f74d214
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-07-04 08:25:49 +0900

    crud_assignment に docstring を記述

--- app/app/crud/crud_assignment.py
+++ app/app/crud/crud_assignment.py
@@ -7,6 +7,23 @@ from app.schemas.user import UserId
 
 
 def find(db: Session, user_id: UserId, task_id: TaskId):
+    """
+    assignments テーブルから１件エントリを取得する。
+
+    Parameters
+    ----------
+    db : sqlalchemy.orm.Session
+        接続するデータベース。
+    user_id: UserId
+        検索するユーザーID。
+    task_id: TaskId
+        検索するタスクID。
+
+    Returns
+    -------
+    ModelAssignment | None
+        検索されたエントリ。
+    """
     return (
         db.query(ModelAssignment)
         .filter(ModelAssignment.user_id == user_id.id)
@@ -16,6 +33,23 @@ def find(db: Session, user_id: UserId, task_id: TaskId):
 
 
 def create(db: Session, assignment: AssignmentCreate):
+    """
+    assignments テーブルに１件エントリを生成する。
+
+    Parameters
+    ----------
+    db : sqlalchemy.orm.Session
+        接続するデータベース。
+    assignment: AssignmentCreate
+        生成するアサインの情報。
+        id は SERIAL によって採番されるので、id の情報は含まない。
+
+    Returns
+    -------
+    ModelAssignment
+        生成されたエントリ。
+        SERIAL によって採番された id の情報を含む。
+    """
     db_assignment = ModelAssignment(user_id=assignment.user_id, task_id=assignment.task_id)
     db.add(db_assignment)
     db.commit()
