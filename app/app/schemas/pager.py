b40943cbc75d1a55cd896145f568c34376a5740f
Kojima Toshiyuki <basabasafoo@gmail.com>
2021-07-04 19:01:46 +0900

    pydantic が Optional であることを要求するので Optional に戻す

--- app/app/schemas/pager.py
+++ app/app/schemas/pager.py
@@ -2,13 +2,15 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
+DEFAULT_PAGE = 1
+DEFAULT_PER_PAGE = 100
 
 class Pager(BaseModel):
-    DEFAULT_PAGE = 1
-    DEFAULT_PER_PAGE = 100
 
-    page: int = Field(DEFAULT_PAGE, ge=1)
-    per_page: int = Field(DEFAULT_PER_PAGE, ge=1, le=500)
+    page: Optional[int] = Field(DEFAULT_PAGE, ge=1)
+    per_page: Optional[int] = Field(DEFAULT_PER_PAGE, ge=1, le=500)
 
     def offset(self):
-        return (self.page - 1) * self.per_page
+        page = self.page if self.page is not None else DEFAULT_PAGE
+        per_page = self.per_page if self.per_page is not None else DEFAULT_PER_PAGE
+        return (page - 1) * per_page
